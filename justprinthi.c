4d665504b3807619fa09d92faaf18998c17f38c5
dancohen447 <dancohen447@gmail.com>
2015-06-05 23:14:30 +0100

    loading module, not finding function

--- justprinthi.c
+++ justprinthi.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main()
+void print()
 {
 	printf("hi\n");
 }
--- main.c
+++ main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 int main(int argc, char **argv)
 {
-	int numberOfArgs = argc-1;
+	void *plugin;
 
-	int args[numberOfArgs];
+	plugin = dlopen("justprinthi.exe", RTLD_NOW);
 
-	// skip the first argument
-	argv++;
+	if(!plugin){
+		printf("fail!\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("plugin loaded\n");
 
-	// while argc > 1 - the number of args have effectively been reduced because the first one is skipped.
-	while(argc-- -1){
-		int position = numberOfArgs - argc;
-		args[position] = atoi(argv[position]);
+	void *print;
+
+	print = dlsym(plugin, "print");
+	if(!print){
+		printf("fail!\n");
+		exit(EXIT_FAILURE);
 	}
 
-	// numberOfArgs is used because sizeof(args) is not usable because of array decay. (???)
-	int i;
-	for (i = 0; i < numberOfArgs; ++i)
-	{
-		printf("arg: %d\n", args[i]);
-	}
-
-    exit(EXIT_SUCCESS);
+	printf("main function loaded\n");
 }
